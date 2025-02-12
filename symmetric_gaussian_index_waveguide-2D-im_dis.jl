d3bbb7cd0857596649431c9086d59ab4fce198d8
Dong Gun Lee <zlffj1032@gmail.com>
2025-02-12 17:29:50 +0900

    im-dis debugging.

--- symmetric_gaussian_index_waveguide-2D-im_dis.jl
+++ symmetric_gaussian_index_waveguide-2D-im_dis.jl
@@ -2,10 +2,11 @@ include("./FD-SBPM-2D-waveguide-PML.jl")
 
 using Serialization
 using .FD_SBPM_2D
+using Printf
 
 function main()
 
-    savedir = joinpath(pwd(),"im-dis-FD_SBPM_2D-PML/")
+    savedir = joinpath(pwd(),"./im-dis-FD_SBPM_2D-PML/")
 
     if !isdir(savedir)
         mkdir(savedir)
@@ -16,21 +17,18 @@ function main()
     nm = 10^-9
 
     Nx = 400
-    Nz = 200
+    Nz = 400
 
     Lx = 20*um
-    Lz = 100*um
+    Lz = 300*um
 
     x = range(-Lx/2, Lx/2; length=Nx)
     z = range(0, Lz; length=Nz)
-
-    im_dis = true
-    if im_dis == true
-        z=1im.*z
-    end
+    τ=1im.*z
 
     dx = step(x)
     dz = step(z)
+    dτ = step(τ)
 
     @show dx / um
     @show dz / um
@@ -42,52 +40,31 @@ function main()
 
     λ = 850*nm
     k0 = 2*π / λ
-    Δβ = 0.12/um
-    @assert abs(Δβ) < (k0*Δn)
-    neff = Δβ/k0 + n0
-    @show neff
-    # β = k0 * ntrial
-    # @show (β-(k0*n0))*um
+    Δntrial = 0.015
+    ntrial = n0 + Δntrial
+    Δβ = Δntrial*k0
+
+    @show ntrial
     @assert abs.(dz) < (λ/2 / n0 / Δn)
+
     w = 2*um
     xshift = 1*um
     Eline = get_gaussian_input(x, xshift, w)
  
     α = 0.5001
 
-    Efield = get_Efield(x, z, neff, n, λ, α, Eline;)
-
     nametag = "Efield"
+
+    mode_transverse_profiles = 
+        get_mode_profiles_im_dis(x, τ, Eline, n, ntrial, 
+                n0, λ, α, 4; savedir=savedir)
 
     serialize(savedir*"x.dat", x)
     serialize(savedir*"z.dat", z)
-    serialize(savedir*nametag, Efield)
 
-    #=
-    Pz, ξ, ξvind, ξv, peakh, Pξ_abs= correlation_method(Efield, dx, dz)
-
-    @show ξv*um
-
-    serialize(savedir*"xiv_$nametag.dat", ξv)
-    serialize(savedir*"xi_$nametag.dat", ξ)
-
-    figname = savedir*"FD_SBPM-2D-waveguide-PML.png"
-    ymax = maximum(Pξ_abs)*1.05
-    serialize(savedir*"correlation_function_abs_max.dat", ymax)
-    plot_withlayout(x, z, Efield, n0, Δn, n, Eline, Pz, ξ, ξv, ξvind, peakh, Pξ_abs, figname; ymax=ymax)
-
-    mode_num = 3
-    Efield = deserialize(savedir*"Efield.dat")
-    ξv = deserialize(savedir*"xiv_Efield.dat")
-    ymax = deserialize(savedir*"correlation_function_abs_max.dat")
-    mode_transverse_profiles = get_h(Lx, Lz, α, mode_num, Efield, n0, Δn, n , λ, ξv; ymax=ymax)
-    # mode_profiles = get_h(Lx, Lz, α, mode_num, Efield, n0, Δn, n , λ, ξv)
-    serialize(savedir*"mode_transverse_profiles.dat", mode_transverse_profiles)
-
-    ξv = deserialize(savedir*"xiv_Efield.dat")
-    mode_transverse_profiles = deserialize(savedir*"mode_transverse_profiles.dat")
-    plot_mode(x, mode_transverse_profiles, ξv, β)
-    =#
+    # filename = @sprintf("im_dis-neff-%.3f-FD_SBPM_2D-noPML.png", neff)
+    # plot_field(x, z, Efield, n0, Δn, n, Eline, 
+                # filename; savedir=savedir)
 
     println("Simulation finished.")
 end
